5f3f35d6175cf19c46ee5a14acb8073b5b717c26
Banovvv <ivan_banov@abv.bg>
2022-08-30 09:02:49 +0300

    EntityTypeConfiguration done

---
+++ Project/Data/Configurations/StateEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SoftServe_IT_Academy.Project.Models;
+
+namespace SoftServe_IT_Academy.Project.Data.Configurations
+{
+    internal class StateEntityTypeConfiguration : IEntityTypeConfiguration<State>
+    {
+        public void Configure(EntityTypeBuilder<State> state)
+        {
+            state.Property(x => x.Name)
+                .IsRequired(true)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        }
+    }
+}
