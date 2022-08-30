a4518e559ea6f7d1a329b3891b923f451b7609af
Banovvv <ivan_banov@abv.bg>
2022-08-30 07:32:09 +0300

    Work on project's db context

---
+++ Project/Models/League.cs
@@ -0,0 +1,12 @@
+﻿namespace SoftServe_IT_Academy.Project.Models
+{
+    public class League
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int SportId { get; set; }
+        public virtual Sport Sport { get; set; }
+    }
+}
---
+++ Project/Models/Sport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace SoftServe_IT_Academy.Project.Models
+{
+    public class Sport
+    {
+        public Sport()
+        {
+            Leagues = new HashSet<League>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<League> Leagues { get; set; }
+    }
+}
