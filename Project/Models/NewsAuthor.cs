45d4119f97f4afbe225359711d85465d4ed956a3
Banovvv <ivan_banov@abv.bg>
2022-08-30 07:48:22 +0300

    Many-to-many tables

---
+++ Project/Models/NewsAuthor.cs
@@ -0,0 +1,11 @@
+﻿namespace SoftServe_IT_Academy.Project.Models
+{
+    public class NewsAuthor
+    {
+        public int NewsId { get; set; }
+        public virtual News News { get; set; }
+
+        public int AuthorId { get; set; }
+        public virtual User Author { get; set; }
+    }
+}
---
+++ Project/Models/NewsTeam.cs
@@ -0,0 +1,11 @@
+﻿namespace SoftServe_IT_Academy.Project.Models
+{
+    public class NewsTeam
+    {
+        public int NewsId { get; set; }
+        public virtual News News { get; set; }
+
+        public int TeamId { get; set; }
+        public virtual Team Team { get; set; }
+    }
+}
