f2d692e083c1c13acae4719f9a77b83c9bf2caaa
Banovvv <ivan_banov@abv.bg>
2022-08-30 08:29:33 +0300

    dbcontext skeleton and packages

---
+++ Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SoftServe_IT_Academy.Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ Project/Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿namespace SoftServe_IT_Academy.Project.Data
+{
+    internal static class Configuration
+    {
+        internal static string GetConnectionString()
+        {
+            // Change 'SportHub' for a different database name
+            return @"Server=.\SQLEXPRESS;Database=SportHub;Integrated Security=true;";
+        }
+    }
+}
