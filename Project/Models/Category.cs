b6880ff3ed092592039cb489295cc62abc673308
Banovvv <ivan_banov@abv.bg>
2022-08-30 07:44:56 +0300

    News-related changes

--- Project/Models/Category.cs
+++ Project/Models/Category.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace SoftServe_IT_Academy.Project.Models
 {
     public class Category
     {
+        public Category()
+        {
+            News = new HashSet<News>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public virtual ICollection<News> News { get; set; }
     }
 }
--- Project/Models/State.cs
+++ Project/Models/State.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace SoftServe_IT_Academy.Project.Models
 {
     public class State
     {
+        public State()
+        {
+            News = new HashSet<News>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public virtual ICollection<News> News { get; set; }
     }
 }
