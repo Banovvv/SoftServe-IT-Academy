b942393e6431b4f600b2ecb065b16c7f1a5af4dc
Banovvv <ivan_banov@abv.bg>
2022-08-30 07:52:01 +0300

    Many-to-many inverse properties

--- Project/Models/News.cs
+++ Project/Models/News.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SoftServe_IT_Academy.Project.Models
 {
     public class News
     {
+        public News()
+        {
+            Teams = new HashSet<NewsTeam>();
+            Authors = new HashSet<NewsAuthor>();
+        }
+
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
@@ -15,5 +22,8 @@ namespace SoftServe_IT_Academy.Project.Models
 
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
+
+        public virtual ICollection<NewsTeam> Teams { get; set; }
+        public virtual ICollection<NewsAuthor> Authors { get; set; }
     }
 }
--- Project/Models/Team.cs
+++ Project/Models/Team.cs
@@ -1,12 +1,21 @@
-﻿namespace SoftServe_IT_Academy.Project.Models
+﻿using System.Collections.Generic;
+
+namespace SoftServe_IT_Academy.Project.Models
 {
     public class Team
     {
+        public Team()
+        {
+            News = new HashSet<NewsTeam>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
         public int LeagueId { get; set; }
         public virtual League League { get; set; }
+
+        public virtual ICollection<NewsTeam> News { get; set; }
     }
 }
