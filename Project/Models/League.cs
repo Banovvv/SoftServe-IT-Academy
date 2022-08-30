05e5e75a6c4fe6ced6206d345b606faa30f43228
Banovvv <ivan_banov@abv.bg>
2022-08-30 07:34:20 +0300

    Legue and Team

--- Project/Models/League.cs
+++ Project/Models/League.cs
@@ -1,12 +1,21 @@
-﻿namespace SoftServe_IT_Academy.Project.Models
+﻿using System.Collections.Generic;
+
+namespace SoftServe_IT_Academy.Project.Models
 {
     public class League
     {
+        public League()
+        {
+            Teams = new HashSet<Team>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
         public int SportId { get; set; }
         public virtual Sport Sport { get; set; }
+
+        public virtual ICollection<Team> Teams { get; set; }
     }
 }
---
+++ Project/Models/Team.cs
@@ -0,0 +1,12 @@
+﻿namespace SoftServe_IT_Academy.Project.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int LeagueId { get; set; }
+        public virtual League League { get; set; }
+    }
+}
