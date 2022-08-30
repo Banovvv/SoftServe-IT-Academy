745aac1ec2e0c2979cb2595123f6fe596f80bb43
Banovvv <ivan_banov@abv.bg>
2022-08-30 07:38:39 +0300

    State and category

---
+++ Project/Models/State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftServe_IT_Academy.Project.Models
+{
+    public class State
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
