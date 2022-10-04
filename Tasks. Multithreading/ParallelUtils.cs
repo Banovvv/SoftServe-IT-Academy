9c2ab41c1283b2f3eb5311b5bb09443c2f9fbc49
Banovvv <ivan_banov@abv.bg>
2022-10-04 16:12:42 +0300

    ParallelUtils

---
+++ Tasks. Multithreading/ParallelUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace SoftServe_IT_Academy.Tasks._Multithreading
+{
+    public class ParallelUtils<T, TR>
+    {
+        private T operand1;
+        private T operand2;
+        private Func<T, T, TR> func;
+
+        public ParallelUtils(Func<T, T, TR> func, T operand1, T operand2)
+        {
+            this.func = func;
+            this.operand1 = operand1;
+            this.operand2 = operand2;
+        }
+
+        public TR Result { get; set; }
+
+        public void StartEvaluation()
+        {
+            Thread thread = new Thread(Evaluate);
+            thread.Start();
+        }
+
+        public void Evaluate() => Result = func(operand1, operand2);
+    }
+}
