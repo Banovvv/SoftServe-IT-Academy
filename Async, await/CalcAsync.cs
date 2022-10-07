73d144aff44ae8a979f15aa2ff802f8e70c8731e
Banovvv <ivan_banov@abv.bg>
2022-10-07 10:08:16 +0300

    TaskPrintSeqAsync

--- Async, await/CalcAsync.cs
+++ Async, await/CalcAsync.cs
@@ -3,12 +3,40 @@ using System.Threading.Tasks;
 
 namespace SoftServe_IT_Academy.Async__await
 {
-    public class CalcAsync
+    static partial class CalcAsync
     {
-        public static async void PrintSeqAsync(int n)
+        public static async Task PrintSeqAsync(int n)
         {
             await Task.Run(() =>
                 Console.WriteLine($"Seq[{n}] = {Calc.Seq(n)}"));
+        }
+
+        public static async Task TaskPrintSeqAsync(int n)
+        {
+            await Task.Run(() =>
+                Console.WriteLine($"Seq[{n}] = {Calc.Seq(n)}"));
+        }
+
+        public static void PrintStatusIfChanged(this Task task, ref TaskStatus prevStatus)
+        {
+            if (prevStatus != task.Status)
+            {
+                Console.WriteLine(task.Status);
+            }
+
+            prevStatus = task.Status;
+        }
+
+        public static void TrackStatus(this Task task)
+        {
+            var status = TaskStatus.Created;
+
+            while (task.Status != TaskStatus.RanToCompletion)
+            {
+                task.PrintStatusIfChanged(ref status);
+            }
+
+            task.PrintStatusIfChanged(ref status);
         }
     }
 
