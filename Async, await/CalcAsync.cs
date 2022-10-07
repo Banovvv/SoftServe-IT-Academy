42a2b07c9a7a1675736ed20a99d78245c45a1601
Banovvv <ivan_banov@abv.bg>
2022-10-07 09:57:29 +0300

    CalcAsync

---
+++ Async, await/CalcAsync.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SoftServe_IT_Academy.Async__await
+{
+    public class CalcAsync
+    {
+        public static async void PrintSeqAsync(int n)
+        {
+            await Task.Run(() =>
+                Console.WriteLine($"Seq[{n}] = {Calc.Seq(n)}"));
+        }
+    }
+
+    // For compiler reasons
+    internal class Calc
+    {
+        internal static int Seq(int n)
+        {
+            return 0;
+        }
+    }
+}
