f1d175fd67cc21b3abb1f6cb6b427fdae01c070c
Banovvv <ivan_banov@abv.bg>
2022-10-07 17:43:37 +0300

    CalcAsyncTask4

---
+++ Async, await/CalcAsyncTask4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SoftServe_IT_Academy.Async__await
+{
+    internal class CalcAsyncTask4
+    {
+        partial class CalcAsync
+        {
+            public async static IAsyncEnumerable<(int, int)> SeqStreamAsync(int n)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    yield return (i, await Task.Run(() => Calc.Seq(i)));
+                }
+            }
+
+            public async static void PrintStream(IAsyncEnumerable<(int, int)> tuple)
+            {
+                await foreach (var t in tuple)
+                {
+                    Console.WriteLine($"Seq[{t.Item1}] = {t.Item2}");
+                }
+            }
+        }
+    }
+}
