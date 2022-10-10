f1273c291599a067244e48ab4e534cd437bd90e2
Banovvv <ivan_banov@abv.bg>
2022-10-10 19:19:17 +0300

    Task 1 skeleton

---
+++ Tasks. Solid/Task1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace SoftServe_IT_Academy.Tasks._Solid
+{
+    public class DbLogger
+    {
+        public void LogMessage(string mMessage)
+        {
+            //Code to write message in database.  
+        }
+    }
+
+    public class FileLogger
+    {
+        public void LogMessage(string mStackTrace)
+        {
+            //code to log stack trace into a file.  
+        }
+    }
+
+    public class ExceptionLogger
+    {
+        public void LogIntoFile(Exception mException)
+        {
+            FileLogger objFileLogger = new FileLogger();
+            objFileLogger.LogMessage(GetUserReadableMessage(mException));
+        }
+        public void LogIntoDataBase(Exception mException)
+        {
+            DbLogger objDbLogger = new DbLogger();
+            objDbLogger.LogMessage(GetUserReadableMessage(mException));
+        }
+        private string GetUserReadableMessage(Exception ex)
+        {
+            string strMessage = string.Empty;
+            //code to convert Exception's stack trace and message to user   
+            // readable format.  
+            return strMessage;
+        }
+    }
+
+    public class DataExporter
+    {
+        public void ExportDataFromFile()
+        {
+            try
+            {
+                //code to export data from files to database.  
+            }
+            catch (IOException ex)
+            {
+                new ExceptionLogger().LogIntoDataBase(ex);
+            }
+            catch (Exception ex)
+            {
+                new ExceptionLogger().LogIntoFile(ex);
+            }
+        }
+    }
+}
