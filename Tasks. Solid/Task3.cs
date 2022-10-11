ddd0fde7c73d2a8630cd102cc50707e7df71b457
Banovvv <ivan_banov@abv.bg>
2022-10-11 16:11:20 +0300

    Task3

---
+++ Tasks. Solid/Task3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SoftServe_IT_Academy.Tasks._Solid
+{
+    public interface ILead
+    {
+        void AssignTask();
+        void CreateSubTask();
+    }
+    public interface IProgrammer
+    {
+        void WorkOnTask();
+    }
+    public class TeamLead : ILead, IProgrammer
+    {
+        public void AssignTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateSubTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WorkOnTask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Manager : ILead
+    {
+        public void AssignTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateSubTask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Programmer : IProgrammer
+    {
+        public void WorkOnTask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
