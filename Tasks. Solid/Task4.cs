3bdd8f82e31280b5228f334f4860153619c9feb0
Banovvv <ivan_banov@abv.bg>
2022-10-11 16:33:25 +0300

    Task4

---
+++ Tasks. Solid/Task4.cs
@@ -0,0 +1,23 @@
+﻿namespace SoftServe_IT_Academy.Tasks._Solid
+{
+    public abstract class Invoice
+    {
+        public abstract double GetDiscount(double amount);
+    }
+    public class OrdinaryInvoice : Invoice
+    {
+        public override double GetDiscount(double amount) => amount - (amount * 1 / 100);
+    }
+    public class FinalInvoice : Invoice
+    {
+        public override double GetDiscount(double amount) => amount - (amount * 3 / 100);
+    }
+    public class ProposedInvoice : Invoice
+    {
+        public override double GetDiscount(double amount) => amount - (amount * 5 / 100);
+    }
+    public class RecurringInvoice : Invoice
+    {
+        public override double GetDiscount(double amount) => amount - (amount * 10 / 100);
+    }
+}
